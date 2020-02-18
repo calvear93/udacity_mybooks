f04c913421a333509a3b3f5cc42f5cee3cf6e481
Cristopher Alvear Candia <c_alvear@live.cl>
2020-02-18 19:46:04 -0300

    Component tweaks

--- src/components/Book.js
+++ src/components/Book.js
@@ -10,13 +10,13 @@ class Book extends React.PureComponent
 
     render()
     {
-        const { id, title, authors, coverUrl } = this.props;
+        const { id, title, authors, coverUrl, shelf } = this.props;
 
         return (
             <div className='book'>
                 <div className='book-top'>
-                    <div className='book-cover' style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }} />
-                    <ShelfBookMenu bookId={id} onChange={this.onChangeBookShelf} />
+                    <div className='book-cover' style={{ width: 128, height: 193, backgroundImage: `url("${coverUrl}")` }} />
+                    <ShelfBookMenu bookId={id} shelf={shelf} onChange={this.onChangeBookShelf} />
                 </div>
                 <div className='book-title'>{title}</div>
                 <div className='book-authors'>{authors}</div>
--- src/components/ShelfBookMenu.js
+++ src/components/ShelfBookMenu.js
@@ -10,13 +10,15 @@ class ShelfBookMenu extends React.PureComponent
 
     render()
     {
+        const { shelf } = this.props;
+
         return (
             <div className='book-shelf-changer'>
                 <select onChange={this.onSelfChange}>
                     <option value='move' disabled>Move to...</option>
-                    <option value='currentlyReading'>Currently Reading</option>
-                    <option value='wantToRead'>Want to Read</option>
-                    <option value='read'>Read</option>
+                    <option value='currentlyReading' disabled={shelf === 'currentlyReading'}>Currently Reading</option>
+                    <option value='wantToRead' disabled={shelf === 'wantToRead'}>Want to Read</option>
+                    <option value='read' disabled={shelf === 'read'}>Read</option>
                     <option value='none'>None</option>
                 </select>
             </div>
